8f6a03f321859e0628a814971dc9eb3cc853d0ea
Alexander Acosta <alexarmonia1@gmail.com>
2026-02-10 22:44:28 -0400

    ejercicio10 mostrar ventana
    aquí creamos un "button" para mostrar una ventana con "messageBox" y aparezca mi nombre y mi matrícula

---
+++ ejercicio 10/ejercicio 10/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ejercicio_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnmostrar = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // btnmostrar
+            // 
+            btnmostrar.Location = new Point(318, 173);
+            btnmostrar.Name = "btnmostrar";
+            btnmostrar.Size = new Size(112, 34);
+            btnmostrar.TabIndex = 0;
+            btnmostrar.Text = "mostrar";
+            btnmostrar.UseVisualStyleBackColor = true;
+            btnmostrar.Click += btnmostrar_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(305, 123);
+            label1.Name = "label1";
+            label1.Size = new Size(140, 25);
+            label1.TabIndex = 1;
+            label1.Text = "HAZ CLIC AQUI:";
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(btnmostrar);
+            Name = "Form1";
+            Text = "mostrar ventana:";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnmostrar;
+        private Label label1;
+    }
+}
---
+++ ejercicio 10/ejercicio 10/Form1.cs
@@ -0,0 +1,20 @@
+namespace ejercicio_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnmostrar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hola soy:\nAlexander Acosta\nSD-19-10375", "presentacion");
+        }
+    }
+}
